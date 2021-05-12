3105298d4feacaf91bf3e340d1b4896ad5d79d1d
MyHorizonn <infernalfonik@gmail.com>
2021-05-12 17:58:25 +0400

    add main page

---
+++ diplom/frontend/src/components/main/main.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+export class Index extends Component{
+    static propTypes = {
+
+    };
+
+    render(){
+        return(
+            <Fragment>
+            </Fragment>
+        )
+    }
+}
+
+export default connect()(Index);
