beb052181fa2d67d3fe302580f05d8e4a8ff16ad
MyHorizonn <infernalfonik@gmail.com>
2021-05-19 10:44:39 +0400

    add machine graphic output

--- diplom/frontend/src/components/machines/machines.js
+++ diplom/frontend/src/components/machines/machines.js
@@ -5,6 +5,41 @@ import { getMachines, delMachines } from '../../actions/machines';
 import {getOrders} from '../../actions/orders';
 
 
+function date_print(order, ord){
+    if(order.hour_or_day == 'DAY'){
+        var date1 = new Date(ord.date_of_order)
+        console.log(date1)
+        date1.setDate(date1.getDate() + order.duration)
+        var date2 = date1.getFullYear() + '-' + (date1.getMonth() + 1) + '-' + date1.getDate()
+        return(
+            <tr>
+                <td>{ord.date_of_order}</td>
+                <td>{ord.order_time}</td>
+                <td>{date2}</td>
+                <td>{ord.order_time}</td>
+            </tr>
+        )
+    }
+    else{
+        var date1 = new Date(ord.date_of_order + 'T' + ord.order_time)
+        console.log(date1)
+        date1.setHours(date1.getHours() + order.duration)
+        console.log(date1)
+        var date2 = date1.getHours() + ':' + date1.getMinutes() + ':' + date1.getSeconds()
+        var date3 = date1.getFullYear() + '-' + (date1.getMonth() + 1) + '-' + date1.getDate()
+        return(
+            <tr>
+                <td>{ord.date_of_order}</td>
+                <td>{ord.order_time}</td>
+                <td>{date3}</td>
+                <td>{date2}</td>
+            </tr>
+        )
+    }
+    
+}
+
+
 export class Machines extends Component {
     static propTypes = {
         machines: PropTypes.array.isRequired,
@@ -25,7 +60,6 @@ export class Machines extends Component {
                 <table className="table">
                     <thead>
                         <tr>
-                            <th>ID</th>
                             <th>Название</th>
                             <th>Описание</th>
                             <th>Стоимость в час</th>
@@ -38,7 +72,6 @@ export class Machines extends Component {
                     <tbody>
                         { this.props.machines.map((machine) =>(
                             <tr key={machine.id}>
-                                <td>{machine.id}</td>
                                 <td>{machine.name}</td>
                                 <td>{machine.about}</td>
                                 <td>{machine.cost_to_hour}</td>
@@ -51,15 +84,14 @@ export class Machines extends Component {
                                                     <table>
                                                     <thead>
                                                         <tr>
-                                                        <th>Дата</th>
-                                                        <th>Время</th>
+                                                            <th>Дата заказа</th>
+                                                            <th>Время заказа</th>
+                                                            <th>Дата окончания</th>
+                                                            <th>Время окончания</th>
                                                         </tr>
                                                     </thead>
-                                                    <tbody>
-                                                        <tr>
-                                                        <td>{ord.date_of_order}</td>
-                                                        <td>{ord.order_time}</td>
-                                                        </tr>
+                                                    <tbody>    
+                                                        {date_print(order, ord)}
                                                     </tbody>
                                                     </table>
                                                 </div>                
